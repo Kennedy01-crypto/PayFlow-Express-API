d2cb5196aa7532f451bce17720f85bdd2f94ebcc
Jim Kennedy <kennedyomata@gmail.com>
2025-11-15 21:58:03 +0300

    feat: add user registration and retrieval endpoints with basic validation

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -24,4 +24,42 @@ router.get("/db", async (req, res) => {
   }
 });
 
+//register user
+// POST request
+// db.collections.("collectionsName").insertOne(qurery)
+router.post("/register", async (req, res) => {
+  try {
+    const db = req.app.locals.db;
+    const collection = db.collection("users");
+    const userData = req.body;
+
+    //simple data validation; email and username
+    if (!userData.email || !userData.username) {
+      res.status(400).json({ message: `Email and username are required` });
+      return;
+    } else {
+      const result = await collection.insertOne(userData);
+      res.status(200).json({ message: `User registered ${result}` });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: `User not registered: ${err}` });
+  }
+});
+
+//get a list of all users
+// GET
+// db.collections.("collectionsName").find().toArray()
+router.get("/users", async (req, res) => {
+  try {
+    const db = req.app.locals.db;
+    const collection = db.collection("users");
+    const users = await collection.find().toArray();
+    res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: `Users not found: ${err}` });
+  }
+});
+
 export default router;

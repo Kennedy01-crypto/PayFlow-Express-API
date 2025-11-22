b7419b15b3ffeb3627e7f041307dc42fec75534c
Jim Kennedy <kennedyomata@gmail.com>
2025-11-22 14:15:47 +0300

    feat: confogured database connection to use mongoose insread of native mongodb Driver

--- src/config/db.js
+++ src/config/db.js
@@ -1,23 +1,14 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+//import Mongoose
+import mongoose from "mongoose";
+
+//conection URI
 const URI = process.env.MONGODB_URI || "mongodb://localhost:27017/payflow-db";
 
 //function to connect to database
 async function DBconnect() {
-  const client = new MongoClient(URI, {
-    ServerAPI: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  });
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-
-    return client;
+    await mongoose.connect(URI);
+    console.log("✅ Connected to database");
   } catch (err) {
     console.error(`Error connecting to database: ${err}`);
     process.exit(1);

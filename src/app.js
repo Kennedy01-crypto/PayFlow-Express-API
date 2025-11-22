44bcb431b330262a388abd4c4852949c965cf515
Jim Kennedy <kennedyomata@gmail.com>
2025-11-22 14:16:29 +0300

    feat: donfigured application startup with the new mongoose configurations

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-//import expree js
+//import express
 import express from "express";
+
+//Daatabase
 import DBconnect from "./config/db.js";
-import ApplicationRputes from "./routes/routes.js";
+import ApplicationRoutes from "./routes/routes.js";
 
 //declare port
 const PORT = process.env.PORT || 8000;
@@ -11,8 +13,11 @@ const app = express();
 // middleware
 app.use(express.json());
 
+//conect to database
+DBconnect();
+
 //routes
-app.use("/api/v1", ApplicationRputes);
+app.use("/api/v1", ApplicationRoutes);
 
 //error handling middelware
 app.use((err, req, res, next) => {
@@ -21,16 +26,7 @@ app.use((err, req, res, next) => {
 });
 
 //app listener using the db connect function
-DBconnect()
-  .then((client) => {
-    console.log("✅ Database connected");
-    app.locals.db = client.db("payflow-db");
-
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-      console.log(`Access the Application at http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error(`❌ Error connecting to database: ${err}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Access the Application at http://localhost:${PORT}`);
+});
